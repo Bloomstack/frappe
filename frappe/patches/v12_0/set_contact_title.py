5a5718c1297233ead7053653712577c60aee8c8f
meshramsaroj <saroj@digithinit.com>
2021-03-22 10:44:06 +0530

    Removed commented code

--- frappe/patches/v12_0/set_contact_title.py
+++ frappe/patches/v12_0/set_contact_title.py
@@ -1,7 +1,6 @@
 import frappe
 
 def execute():
-    print("===============this is patch=======")
     frappe.reload_doc("Contacts", "doctype", "Contact")
     contacts = frappe.get_all("Contact",["name","title"])
     for contact in contacts:
