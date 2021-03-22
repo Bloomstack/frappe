982253384443e2e68a0ff14ae69687b3f4e5b04b
meshramsaroj <saroj@digithinit.com>
2021-03-22 10:54:19 +0530

    Made changes in patch file

--- frappe/patches/v12_0/set_contact_title.py
+++ frappe/patches/v12_0/set_contact_title.py
@@ -1,12 +1,8 @@
 import frappe
 
 def execute():
-    frappe.reload_doc("Contacts", "doctype", "Contact")
-    contacts = frappe.get_all("Contact",["name","title"])
-    for contact in contacts:
-        frappe.db.sql("""
-                        UPDATE
-                            `tab%s`
-                        SET
-				            contact.title = contact.name
-            """% (contact))
+	frappe.reload_doc("contacts", "doctype", "contact")
+	frappe.db.sql("""
+		UPDATE `tabContact`
+		SET title = name
+	""")
