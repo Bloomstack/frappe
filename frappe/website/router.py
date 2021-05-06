b4539c65480507f0ea24632b3c73b2580a3a37dd
Suraj Shetty <surajshetty3416@gmail.com>
2021-05-06 14:52:32 +0530

    fix: Handle exception

--- frappe/website/router.py
+++ frappe/website/router.py
@@ -153,7 +153,7 @@ def evaluate_dynamic_routes(rules, path):
 	route_map = Map(rules)
 	endpoint = None
 
-	if frappe.local.request and frappe.local.request.environ:
+	if hasattr(frappe.local, 'request') and frappe.local.request.environ:
 		urls = route_map.bind_to_environ(frappe.local.request.environ)
 		try:
 			endpoint, args = urls.match("/" + path)
