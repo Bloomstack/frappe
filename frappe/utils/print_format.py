1774aaace7263a99f0b66e53b7992a91d3377548
vishdha <vishaldhayagude07@gmail.com>
2021-05-11 21:51:19 +0530

    fix: minor change added

--- frappe/utils/print_format.py
+++ frappe/utils/print_format.py
@@ -88,9 +88,10 @@ def read_multi_pdf(output):
 @frappe.whitelist()
 def download_pdf(doctype, name, format=None, doc=None, no_letterhead=0):
 	html = frappe.get_print(doctype, name, format, doc=doc, no_letterhead=no_letterhead)
-	doc = frappe.get_doc(doctype, name)
-	if doc.title:
-		name = name +"-"+doc.title
+	title_field = frappe.get_meta(doctype).title_field
+	title = frappe.get_value(doctype, name, title_field)
+	if title:
+		name = "-".join([name, title])
 	frappe.local.response.filename = "{name}.pdf".format(name=name.replace(" ", "-").replace("/", "-"))
 	frappe.local.response.filecontent = get_pdf(html)
 	frappe.local.response.type = "pdf"
