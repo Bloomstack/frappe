c673f517984e6ead29d7c3e1d3f494d38c1ddeb4
vishdha <vishaldhayagude07@gmail.com>
2021-05-11 20:19:04 +0530

    feat: save document with title field

--- frappe/utils/print_format.py
+++ frappe/utils/print_format.py
@@ -88,6 +88,9 @@ def read_multi_pdf(output):
 @frappe.whitelist()
 def download_pdf(doctype, name, format=None, doc=None, no_letterhead=0):
 	html = frappe.get_print(doctype, name, format, doc=doc, no_letterhead=no_letterhead)
+	doc = frappe.get_doc(doctype, name)
+	if doc.title:
+		name = name +"-"+doc.title
 	frappe.local.response.filename = "{name}.pdf".format(name=name.replace(" ", "-").replace("/", "-"))
 	frappe.local.response.filecontent = get_pdf(html)
 	frappe.local.response.type = "pdf"
