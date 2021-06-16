74fc5c316bc12bfea9dba9fa4acf486b6faf02a6
Neha Sacher <nehasacher143@gmail.com>
2021-06-16 16:30:58 +0530

    fix: sider issue

--- frappe/public/js/frappe/utils/utils.js
+++ frappe/public/js/frappe/utils/utils.js
@@ -621,7 +621,7 @@ Object.assign(frappe.utils, {
 	format_phone: function(value, doc) {
 		let country_code = frappe.boot.default_country.code;
 
-		if (doc.hasOwnProperty("country_code")) {
+		if (Object.prototype.hasOwnProperty.call(doc, "country_code")) {
 			country_code = doc.country_code;
 		}
 
