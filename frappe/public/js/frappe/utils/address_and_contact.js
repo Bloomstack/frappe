e49de66e5bea680e19426a271b8381c5a213c7ea
Myuddin khatri <myuddin@digithinkit.com>
2021-06-30 11:43:24 +0530

    fix: sider issues for addresss_and_contact.js

--- frappe/public/js/frappe/utils/address_and_contact.js
+++ frappe/public/js/frappe/utils/address_and_contact.js
@@ -8,34 +8,34 @@ $.extend(frappe.contacts, {
 
 
 
-	paginate: function(total_items, pagination_class,html_class, box_class) {
+	paginate: function (total_items, pagination_class, html_class, box_class) {
 		let per_page = 3;
-		let count = Math.ceil(total_items/per_page);
-		if($('body').has(`.${pagination_class}`).length) {
+		let count = Math.ceil(total_items / per_page);
+		if ($('body').has(`.${pagination_class}`).length) {
 			$(`.${pagination_class}`).empty();
 		}
 		else {
 			$(`div[title="${html_class}"]`).append(`<div class = "${pagination_class}"></div>`);
 		}
-		for(var i=1; i<=count; i++){
-			$(`.${pagination_class}`).append("<div class='page_number'>"+i+"</div>");
+		for (var i = 1; i <= count; i++) {
+			$(`.${pagination_class}`).append("<div class='page_number'>" + i + "</div>");
 		}
 		$(`.${pagination_class} .page_number:first-child`).addClass('active');
-		$(`div[data-fieldname=${html_class}] .${box_class}`).filter(function(index) {
+		$(`div[data-fieldname=${html_class}] .${box_class}`).filter(function (index) {
 			return index < per_page;
 		}).addClass('active');
-	
-		$(`body`).on('click',`.${pagination_class} .page_number`,function(){
+
+		$(`body`).on('click', `.${pagination_class} .page_number`, function () {
 			var page_index = $(this).index();
-			var start=page_index*per_page;
+			var start = page_index * per_page;
 			$(`div[data-fieldname=${html_class}] .${box_class}`).removeClass('active');
 			$(`.${pagination_class} .page_number`).removeClass('active');
 			$(`.${pagination_class} .page_number`).eq(page_index).addClass('active');
-			for(var j=0;j<per_page;j++){
-			  $(`div[data-fieldname=${html_class}] .${box_class}`).eq(start+j).addClass('active');
+			for (var j = 0; j < per_page; j++) {
+				$(`div[data-fieldname=${html_class}] .${box_class}`).eq(start + j).addClass('active');
 			}
 		});
-	
+
 	},
 
 	render_address_and_contact: function (frm) {
@@ -48,23 +48,23 @@ $.extend(frappe.contacts, {
 					if (layout_for_address.get_values().search_address) {
 						frappe.call({
 							"method": "frappe.contacts.address_and_contact.get_addresses",
-							"args":{
+							"args": {
 								"doc": frm.doc,
 								"search_keyword": layout_for_address.get_values().search_address
 							},
 							callback: function (res) {
 								frappe.contacts.clear_address_and_contact(frm);
-								if(res.message) {
-									frm.doc.__onload.addr_list = res.message
+								if (res.message) {
+									frm.doc.__onload.addr_list = res.message;
 									frappe.contacts.render_address_and_contact(frm);
-									frm.refresh_field("address_html1")
+									frm.refresh_field("address_html1");
 								}
 							}
 						})
 					}
 					else {
 						frappe.contacts.clear_address_and_contact(frm);
-						frappe.contacts.render_address_and_contact(frm)
+						frappe.contacts.render_address_and_contact(frm);
 					}
 				}
 			},
@@ -76,7 +76,7 @@ $.extend(frappe.contacts, {
 		];
 		var layout_for_address = new frappe.ui.FieldGroup({
 			body: frm.get_field('address_html').$wrapper,
-			fields:address_fields,
+			fields: address_fields,
 		});
 		// render address
 		if (frm.fields_dict['address_html'] && "addr_list" in frm.doc.__onload) {
@@ -86,28 +86,28 @@ $.extend(frappe.contacts, {
 				.find(".btn-address").on("click", function () {
 					frappe.new_doc("Address");
 				});
-				layout_for_address.make();
-				layout_for_address.set_input("address_html1", frappe.render_template("address_list",frm.doc.__onload))
-			}
-			
+			layout_for_address.make();
+			layout_for_address.set_input("address_html1", frappe.render_template("address_list", frm.doc.__onload))
+		}
+
 		let no_of_addresses = frm.doc.__onload.addr_list.length;
 		frappe.contacts.paginate(no_of_addresses, "address_pagination", "address_html1", "address-box");
 		$(document).on('click', '.btn-address-link', function () {
 			frappe.prompt({
-					label: "Link Address",
-					fieldname: "address",
-					fieldtype: "Link",
-					options: "Address",
-					reqd: 1,
-					get_query: () => {
-						let addr_list = frm.doc.__onload.addr_list.map(addr => addr.name);
-						return {
-							filters: {
-								"name": ["not in", addr_list]
-							}
+				label: "Link Address",
+				fieldname: "address",
+				fieldtype: "Link",
+				options: "Address",
+				reqd: 1,
+				get_query: () => {
+					let addr_list = frm.doc.__onload.addr_list.map(addr => addr.name);
+					return {
+						filters: {
+							"name": ["not in", addr_list]
 						}
 					}
-				},
+				}
+			},
 				(data) => {
 					frappe.call({
 						method: "frappe.contacts.address_and_contact.link_address_or_contact",
@@ -138,7 +138,7 @@ $.extend(frappe.contacts, {
 							"name": cur_frm.docname
 						},
 						callback: function () {
-							window.location.reload()
+							window.location.reload();
 						}
 					})
 				}
@@ -160,7 +160,7 @@ $.extend(frappe.contacts, {
 							"name": cur_frm.docname
 						},
 						callback: function () {
-							window.location.reload()
+							window.location.reload();
 						}
 					})
 				}
@@ -176,23 +176,23 @@ $.extend(frappe.contacts, {
 					if (layout_for_contacts.get_values().search_contact) {
 						frappe.call({
 							"method": "frappe.contacts.address_and_contact.get_contacts",
-							"args":{
+							"args": {
 								"doc": frm.doc,
 								"search_keyword": layout_for_contacts.get_values().search_contact
 							},
 							callback: function (res) {
 								frappe.contacts.clear_address_and_contact(frm);
-								if(res.message) {
-									frm.doc.__onload.contact_list = res.message
+								if (res.message) {
+									frm.doc.__onload.contact_list = res.message;
 									frappe.contacts.render_address_and_contact(frm);
-									frm.refresh_field("contact_html1")
+									frm.refresh_field("contact_html1");
 								}
 							}
 						})
 					}
 					else {
 						frappe.contacts.clear_address_and_contact(frm);
-						frappe.contacts.render_address_and_contact(frm)
+						frappe.contacts.render_address_and_contact(frm);
 					}
 				}
 			},
@@ -210,32 +210,32 @@ $.extend(frappe.contacts, {
 		if (frm.fields_dict['contact_html'] && "contact_list" in frm.doc.__onload) {
 			$(frm.fields_dict['contact_html'].wrapper)
 				// .html(frappe.render_template("contact_list",
-					// frm.doc.__onload))
+				// frm.doc.__onload))
 				.find(".btn-contact").on("click", function () {
 					frappe.new_doc("Contact");
-				}, 
+				},
 				);
-				layout_for_contacts.make()
-				layout_for_contacts.set_input("contact_html1", frappe.render_template("contact_list",frm.doc.__onload))
+			layout_for_contacts.make()
+			layout_for_contacts.set_input("contact_html1", frappe.render_template("contact_list", frm.doc.__onload))
 		}
 		let no_of_contacts = frm.doc.__onload.contact_list.length;
 		frappe.contacts.paginate(no_of_contacts, "contact_pagination", "contact_html1", "contact-box");
 		$(document).on('click', '.btn-contact-link', function () {
 			frappe.prompt({
-					label: "Link Contact",
-					fieldname: "contact",
-					fieldtype: "Link",
-					options: "Contact",
-					reqd: 1,
-					get_query: () => {
-						let contact_list = frm.doc.__onload.contact_list.map(contact => contact.name);
-						return {
-							filters: {
-								"name": ["not in", contact_list]
-							}
+				label: "Link Contact",
+				fieldname: "contact",
+				fieldtype: "Link",
+				options: "Contact",
+				reqd: 1,
+				get_query: () => {
+					let contact_list = frm.doc.__onload.contact_list.map(contact => contact.name);
+					return {
+						filters: {
+							"name": ["not in", contact_list]
 						}
 					}
-				},
+				}
+			},
 				(data) => {
 					frappe.call({
 						method: "frappe.contacts.address_and_contact.link_address_or_contact",
@@ -246,7 +246,7 @@ $.extend(frappe.contacts, {
 							"link_name": frm.docname
 						},
 						callback: function () {
-							window.location.reload()
+							window.location.reload();
 						}
 					})
 				}, __("Select Contact"));
@@ -266,7 +266,7 @@ $.extend(frappe.contacts, {
 							"name": cur_frm.docname
 						},
 						callback: function () {
-							window.location.reload()
+							window.location.reload();
 						}
 					})
 				}
@@ -288,12 +288,11 @@ $.extend(frappe.contacts, {
 							"name": cur_frm.docname
 						},
 						callback: function () {
-							window.location.reload()
+							window.location.reload();
 						}
 					})
 				}
 			);
 		});
-
 	}
 })
