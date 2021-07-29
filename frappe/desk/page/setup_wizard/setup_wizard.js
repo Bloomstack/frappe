c5a85c0ca0b8fa69922a573f4f1f39ef8028e83e
sahil28297 <37302950+sahil28297@users.noreply.github.com>
2021-07-29 14:02:25 +0530

    Update frappe/desk/page/setup_wizard/setup_wizard.js

--- frappe/desk/page/setup_wizard/setup_wizard.js
+++ frappe/desk/page/setup_wizard/setup_wizard.js
@@ -385,7 +385,7 @@ frappe.setup.slides_settings = [
 		icon: "fa fa-flag",
 		// help: __("Select your Country, Time Zone and Currency"),
 		fields: [
-			{ fieldname: "country", label: __("Your Country"), reqd:1, fieldtype: "Select" },
+			{ fieldname: "country", label: __("Your Country"), reqd: 1, fieldtype: "Select" },
 			{ fieldtype: "Section Break" },
 			{ fieldname: "timezone", label: __("User's Default Time Zone"), reqd:1, fieldtype: "Select" },
 			{ fieldtype: "Column Break" },
