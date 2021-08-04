d92991d5d3125eb6cdadc48692c1b4d5d673bb0f
Myuddin khatri <myuddin@digithinkit.com>
2021-08-04 17:57:19 +0530

    fix: fixed sider issues for address_and_contact.js

--- frappe/public/js/frappe/utils/address_and_contact.js
+++ frappe/public/js/frappe/utils/address_and_contact.js
@@ -279,8 +279,8 @@ $.extend(frappe.contacts, {
 	get_last_doc: function(frm) {
 		const reverse_routes = frappe.route_history.reverse();
 		const last_route = reverse_routes.find(route => {
-			return route[0] === 'Form' && route[1] !== frm.doctype
-		})
+			return route[0] === 'Form' && route[1] !== frm.doctype;
+		});
 		let doctype = last_route && last_route[1];
 		let docname = last_route && last_route[2];
 
@@ -290,6 +290,6 @@ $.extend(frappe.contacts, {
 		return {
 			doctype,
 			docname
-		}
+		};
 	}
 })
